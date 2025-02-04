a4761aa98dd3b9ef6a9b96d554f735907d7c0775
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-02-04 17:53:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,10 @@ const getRange = (array) => {
   return Math.max(...array) - Math.min(...array);
 }
 
-
+const getVariance = (array)=>{
+  const mean = getMean(array);
+  const differences = array.map((el)=> el - mean);
+}
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
