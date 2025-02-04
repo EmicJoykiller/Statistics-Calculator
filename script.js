f3df6db6ea2bf0eb01963fa671ceeef141dfa884
EmicJoykiller <152107345+EmicJoykiller@users.noreply.github.com>
2025-02-04 17:26:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-  const sorted = array.sort((a, b) => a - b);
+  const sorted = array.toSorted((a, b) => a - b);
+  const length = sorted.length;
+  const middle = Math.floor(length / 2);
+
+  return length % 2 === 0 
+    ? (sorted[middle - 1] + sorted[middle]) / 2 
+    : sorted[middle];
 }
 
 const calculate = () => {
@@ -10,6 +16,8 @@ const calculate = () => {
   const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
   
   const mean = getMean(numbers);
+  const median = getMedian(numbers);
 
   document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
 }
